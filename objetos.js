2a344d797e692b1449f8cb0121e95cd36b6e528b
Vitor Oliveira <victorskw89@gmail.com>
2023-10-10 15:40:33 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,38 @@
+   // defindindo objeto 
+       const pessoa = {
+        nome: 'Luiz',
+        sobrenome: 'Oliveira',
+        idade: 34,
+        signo: 'peixes',
+        altura: 1.78,
+        nacionalidade: 'kanadense',
+        curso: 'publicidade',
+        //um objeto dentro e ouro
+        endereco: {
+           rua: 'Av Brasil', 
+           numero: 440,
+        }
+    } 
+
+    console.log(pessoa);
+    // usando a notação de ponto (.) para atribuição normal 
+    console.log(pessoa.nome);
+    
+    console.log(pessoa.signo);
+    
+    console.log(pessoa.idade);
+    
+    console.log(pessoa.altura);
+    
+    // jogar numa variável 
+    
+    //const nome = pessoa.nome;
+    //console.log(nome);
+    
+    // atribuição via desestruturação usando Chaves {}
+    
+    const { nome, sobrenome, idade } = pessoa;
+    console.log(nome + " " + sobrenome, idade);
+    
+    const {endereco: { rua, numero}} = pessoa;
+    console.log(rua, numero);
