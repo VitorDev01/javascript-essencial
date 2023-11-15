18593eed50e8bfb091de6236d2d47417a9bb01a1
Vitor Oliveira <victorskw89@gmail.com>
2023-11-15 20:14:46 -0300

    Create TiposDeSaida.js

---
+++ TiposDeSaida.js
@@ -0,0 +1,12 @@
+// getElementById : método que seleciona determinado elemento pelo seu id no html
+// INNERHTML é a primeira forma de saida que tem no javascript mostrando um conteúdo html
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!":
+
+// O document write tem a mesma função do innerHtml
+document.write("HELLO WORLD!!");
+
+//caixa de alerta 
+alert(10+15): 
+
+//console
+console.log("olha no console");
